eff193667c46d417eb721bf203e38652a3644fce
Shawn Miller <hereiamnow@gmail.com>
2025-07-22 11:21:44 -0500

    Added file header with last update.

--- src/components/Drawers/index.js
+++ src/components/Drawers/index.js
@@ -1,3 +1,11 @@
+// File: index.js
+// Path: src/components/Drawers/index.js
+// Project: Humidor Hub
+// Author: Shawn Miller (hereiamnow@gmail.com)
+// Date: July 21, 2025
+// Time: 10:30 AM
+// Description: Export index for all drawer components
+
 export { default as BrowseByWrapperDrawer } from './BrowseByWrapperDrawer';
 export { default as BrowseByStrengthDrawer } from './BrowseByStrengthDrawer';
 export { default as BrowseByCountryDrawer } from './BrowseByCountryDrawer';
